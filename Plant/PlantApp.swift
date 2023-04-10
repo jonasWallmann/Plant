7a0dbefd8aefed43469ba713da539d45da5f33f1
Jonas Wallmann <jwallmann@newstore.com>
2023-04-10 15:36:14 +0200

    Initial Commit

---
+++ Plant/PlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantApp.swift
+//  Plant
+//
+//  Created by Jonas Wallmann on 10.04.23.
+//
+
+import SwiftUI
+
+@main
+struct PlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
